0ffba33c625850c84efe094145cd9b806cf36001
malloryrbush <malloryrbush@hotmail.com>
2018-08-18 19:35:51 -0400

    first commit

---
+++ PracticeMVC/Models/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PracticeMVC.Models
+{
+    public class Class1
+    {
+        [Key]
+        public int ClassID { get; set; }
+        public string Name { get; set; }
+        public int MyAge { get; set; }
+    }
+}
